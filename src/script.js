8dad7649618f985cd07e49886be5b3ae487645c0
BrakeLJ <lukejbrake@gmail.com>
2020-10-14 17:12:18 -0400

    more layout and styling

--- src/script.js
+++ src/script.js
@@ -7,6 +7,8 @@ window.addEventListener('scroll', function () {
   nav.classList.toggle('nav__scroll', scrollPosition);
 })
 
+const scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
+console.log(scrollPos);
 
 
 // document.addEventListener('DOMContentLoaded', function () {
